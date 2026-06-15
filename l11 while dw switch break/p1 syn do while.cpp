41dbaf640ce188a82a46e247daafb032f1cc6c53
Himanshu Joshi <iamhjoc@gmail.com>
2026-06-15 18:19:02 +0530

    Add files via upload

---
+++ l11 while dw switch break/p1 syn do while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+//this program display number from 1 to n 
+int main()
+
+{ int n,i=1 ;
+    
+    cout<<"Enter the number"<<endl;
+    cin>>n;
+    cout<<endl;
+;// do nothing
+
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=n);
+
+;// a semicolon ; is itself a ending statement
+ // but they made hide a bug for if for while 
+
+return 0;
+}
