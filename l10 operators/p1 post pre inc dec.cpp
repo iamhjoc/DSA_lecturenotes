755d198b0d8f2f5056548464e324caa49a43b934
Himanshu Joshi <iamhjoc@gmail.com>
2026-06-16 23:56:55 +0530

    Add files via upload

---
+++ l10 operators/p1 post pre inc dec.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//this program display number from 1 to n 
+int main()
+
+{ int a ;
+    
+    cout<<"Enter the number"<<endl;
+    cin>>a;
+     cout<<a++;
+
+return 0;
+}
