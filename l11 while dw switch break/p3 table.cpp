1eef08398e43d52d211afcb82fde5c2ad0552c4b
Himanshu Joshi <iamhjoc@gmail.com>
2026-06-19 23:50:05 +0530

    Add files via upload

---
+++ l11 while dw switch break/p3 table.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{ int n = 8 ;
+    int i = 1;
+    while(i<=10)
+    {
+        cout<<n*i<<" ";
+      
+  //cout<<endl;
+        i++;
+         //cout<<endl;
+         //both endl will give same output 
+        
+    }
+
+
+
+
+
+return 0;
+}
