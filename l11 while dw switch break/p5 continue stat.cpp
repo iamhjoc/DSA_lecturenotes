dc9ddba7f19a8f36c6935c971e2132a2b5f17fe2
Himanshu Joshi <iamhjoc@gmail.com>
2026-06-21 22:49:55 +0530

    Add files via upload

---
+++ l11 while dw switch break/p5 continue stat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{ int i ;
+    for(i=0;i<=30;i++){
+        cout<<endl;
+        if(i%5==0)//logic any number which is completetly divisble by 5 
+    continue;
+    cout<< i;
+    
+    
+    }
+return 0;
+}
