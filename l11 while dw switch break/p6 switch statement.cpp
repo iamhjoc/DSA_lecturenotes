63bd5d4ee5c662bc4ac36125b7842d86617fa531
Himanshu Joshi <iamhjoc@gmail.com>
2026-06-23 01:03:25 +0530

    Add keyboard event handling code as comments
    Added commented JavaScript code for handling keyboard events.

--- l11 while dw switch break/p6 switch statement.cpp
+++ l11 while dw switch break/p6 switch statement.cpp
@@ -17,6 +17,37 @@ break;
 
 
 }
+// js actual code use to make flash cards in website in js is :
+// document.addEventListener('keydown', function(e) {
+//     if (e.target && (e.target.tagName === 'SELECT' || e.target.tagName === 'INPUT'))
+//         return;
+//     switch (e.key) {
+//     case ' ':
+//         e.preventDefault();
+//         flip();
+//         break;
+//     case 'n':
+//     case 'N':
+//         next();
+//         break;
+//     case 'p':
+//     case 'P':
+//         prev();
+//         break;
+//     case 's':
+//     case 'S':
+//         shuffle();
+//         break;
+//     case '1':
+//         if (flipped)
+//             mark('know');
+//         break;
+//     case '2':
+//         if (flipped)
+//             mark('learn');
+//         break;
+//     }
+// });
 
 return 0;
 }
