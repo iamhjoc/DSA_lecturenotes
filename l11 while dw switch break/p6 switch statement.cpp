ac7ded046e1eaae484b1edada3531c92eb19c587
Himanshu Joshi <iamhjoc@gmail.com>
2026-06-22 23:51:28 +0530

    Add files via upload

---
+++ l11 while dw switch break/p6 switch statement.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int i ;
+cout<<"Enter the number of day : ";
+switch (i){
+
+case 1:
+cout<<"Mon";
+break;
+
+case 2:
+cout<<"Tue";
+break;
+
+
+}
+
+return 0;
+}
