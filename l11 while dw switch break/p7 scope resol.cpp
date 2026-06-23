8f9a81071eb495ce5edec07c651dcfdfdac57387
Himanshu Joshi <iamhjoc@gmail.com>
2026-06-23 22:33:00 +0530

    Add files via upload

---
+++ l11 while dw switch break/p7 scope resol.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{ int z ;
+    cout<<"Enter value you want to store in z";
+    cin>>z;
+    cout<<z;
+int z;//two same variable are not allowed in same block
+return 0;
+}
+// int num=11;
+// int n=11;
+// if(n==10)
+// {
+//     int num =20;//but in different block two same variables are allowed in coed thats called scope of variable
+// }
