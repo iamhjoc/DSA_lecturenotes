d6b7b8969bf8799ff1ecfe5985532aeb37e82824
Himanshu Joshi <iamhjoc@gmail.com>
2026-06-27 23:02:17 +0530

    Add leap year calculation program in C++

--- l13 lc easy/gfg leap yr.cpp
+++ l13 lc easy/gfg leap yr.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
-
+//logic if you want that number should divide completely so the remainder should be 0 so use modulus operator
 int main() {
     int y;
     cout<<"Enter the year : ";
     cin>>y;
 
-    if(y/400==int )
-    {
+    if(y%400==0 )
+    { 
         cout<<"your entered number is a leap year";
     }
-    if(y/4==int && y/4=float) {
+    else if( y%4==0 && y%100  !=  0) {
      
          cout<<"your entered number is a leap year";
     }
