f180801180d7d28b86386b388a88b93c68982dcb
Himanshu Joshi <iamhjoc@gmail.com>
2026-06-28 23:39:40 +0530

    Add files via upload

---
+++ l13 lc easy/rev int.cpp
@@ -0,0 +1,19 @@
+        #include <iostream>
+using namespace std;
+
+int main() {
+    int i , output;
+    cout<<"Enter the integer : ";
+    cin>>  i;
+    while(i != 0)// after few iteration i /10 will give 0 so if 0 loop stops 
+    {  output =  i%10;
+      //ans = ans * 10 + rem l12 logic and rem is n%10 
+ 
+       }
+
+    cout<<output;
+
+
+    
+    return 0;
+}
