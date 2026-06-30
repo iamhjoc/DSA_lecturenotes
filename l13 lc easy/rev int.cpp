f56833d6cc0d9e8ecfb5972166f889e8529883e4
Himanshu Joshi <iamhjoc@gmail.com>
2026-06-30 22:06:17 +0530

    Fix integer reversal logic and add overflow check
    Refactor integer reversal logic to handle overflow and improve readability.

--- l13 lc easy/rev int.cpp
+++ l13 lc easy/rev int.cpp
@@ -1,17 +1,25 @@
         #include <iostream>
+        #include <climits>
 using namespace std;
 
 int main() {
-    int i , output;
+    int i , rem ,ans =0;
     cout<<"Enter the integer : ";
     cin>>  i;
     while(i != 0)// after few iteration i /10 will give 0 so if 0 loop stops 
-    {  output =  i%10;
-      //ans = ans * 10 + rem l12 logic and rem is n%10 
- 
+    { 
+      rem =  i%10;
+      i/=10;
+      if (ans>INT_MAX/10 || ans<INT_MIN/10)
+      {
+        return 0;
+      }
+      ans= ans*10 + rem;  //ans = ans * 10 + rem l12 logic and rem is n%10 
+      
+
        }
 
-    cout<<output;
+    cout<<"Reversed integer : "<<ans;
 
 
     
