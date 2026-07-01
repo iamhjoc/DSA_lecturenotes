64064450bf310eaab759b04d03ca9abf6a3e647e
Himanshu Joshi <iamhjoc@gmail.com>
2026-07-01 23:51:58 +0530

    Add files via upload

---
+++ l13 lc easy/lc231.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter any number";
+    cin>>n;
+    if(n>1)
+    return 0;
+
+    while(n!=1)
+    {
+        if(n%2==1)
+        return 0;
+
+        n=n/2;
+    }
+cout<<"This number is power of 2"<<n;
+       return 0;
+}
